75b838ae2a7b6a396a67f0766f30628e2e583061
Jason Wu <jasonwu830731@gmail.com>
2016-11-02 21:22:39 +0800

    calculator

--- src/components/CalcApp.js
+++ src/components/CalcApp.js
@@ -7,15 +7,83 @@ class CalcApp extends React.Component {
     super(props);
     this.state = {
       // TODO
+      operator: '+',
+      memory: 0,
+      current_input: '',
+      display_value:'0',
+      last_is_equal: 0,
     };
+    this.keyInNumber = this.keyInNumber.bind(this);
+    this.keyInOperator = this.keyInOperator.bind(this);
+    this.keyInEqual = this.keyInEqual.bind(this);
   }
 
   resetState() {
     // TODO
+    this.setState({ display_value: '0' });
+    this.setState({ operator: '+' });
+    this.setState({ current_input: '' });
+    this.setState({ memory: 0 });
+    this.setState({ last_is_equal: 0 });
   }
 
   showNotImplemented() {
     console.warn('This function is not implemented yet.');
+  }
+
+  keyInNumber(number) {
+    if(this.state.last_is_equal){
+      this.setState({ display_value: number });
+    }else{
+      this.setState({ display_value: this.state.current_input.concat([number]) });
+      this.setState({ current_input: this.state.current_input.concat([number]) });
+    }
+  }
+
+  keyInOperator(op) {
+    if (this.state.current_input != '') {
+      const op_old = this.state.operator
+      let temp = this.state.memory
+      if (this.state.last_is_equal == 0){
+        if(op_old == '+')
+          temp = temp + parseFloat(this.state.current_input)
+        else if(op_old == '-')
+          temp = temp - parseFloat(this.state.current_input)
+        else if(op_old == 'x')
+          temp = temp * parseFloat(this.state.current_input)
+        else if(op_old == '÷')
+          temp = temp / parseFloat(this.state.current_input)
+        else {
+          console.log('Fuck keyInOperator')
+        }
+      }
+      this.setState({ memory: temp });
+      this.setState({ operator: op });
+      this.setState({ display_value: temp });
+      this.setState({ current_input: '' });
+    } else {
+      this.setState({ operator: op });
+    }  
+    this.setState({ last_is_equal: 0 });
+  }
+
+  keyInEqual(equal) {
+    const op = this.state.operator
+    let temp = this.state.memory
+    if(op == '+')
+      temp = temp + parseFloat(this.state.current_input)
+    else if(op == '-')
+      temp = temp - parseFloat(this.state.current_input)
+    else if(op == 'x')
+      temp = temp * parseFloat(this.state.current_input)
+    else if(op == '÷')
+      temp = temp / parseFloat(this.state.current_input)
+    else {
+      console.log('Fuck keyInOperator')
+    }
+    this.setState({ memory: temp });
+    this.setState({ display_value: temp });
+    this.setState({ last_is_equal: 1 });
   }
 
   render() {
@@ -23,19 +91,36 @@ class CalcApp extends React.Component {
       <div className="calc-app">
         <div className="calc-container">
           <div className="calc-output">
-            <div className="calc-display">1980</div>
+            <div className="calc-display">{this.state.display_value}</div>
           </div>
           <div className="calc-row">
             <CalcButton onClick={this.resetState.bind(this)}>AC</CalcButton>
             <CalcButton onClick={this.showNotImplemented.bind(this)}>+/-</CalcButton>
             <CalcButton onClick={this.showNotImplemented.bind(this)}>%</CalcButton>
-            <CalcButton className="calc-operator">÷</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.keyInOperator}>÷</CalcButton>
           </div>
           <div className="calc-row">
-            <CalcButton className="calc-number">7</CalcButton>
-            <CalcButton className="calc-number"></CalcButton>
-            <CalcButton className="calc-number"></CalcButton>
-            <CalcButton className="calc-operator"></CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>7</CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>8</CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>9</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.keyInOperator}>x</CalcButton>
+          </div>
+          <div className="calc-row">
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>4</CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>5</CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>6</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.keyInOperator}>-</CalcButton>
+          </div>
+          <div className="calc-row">
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>1</CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>2</CalcButton>
+            <CalcButton className="calc-number" onClick={this.keyInNumber}>3</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.keyInOperator}>+</CalcButton>
+          </div>
+          <div className="calc-row">
+            <CalcButton className="calc-number bigger-btn" onClick={this.keyInNumber}>0</CalcButton>
+            <CalcButton className="calc-number">.</CalcButton>
+            <CalcButton className="calc-operator" onClick={this.keyInEqual}>=</CalcButton>
           </div>
         </div>
       </div>
--- src/components/__tests__/CalcApp.spec.js
+++ src/components/__tests__/CalcApp.spec.js
@@ -53,7 +53,7 @@ it('7 8 9 -> 789', () => {
   btn8.simulate('click');
   btn9.simulate('click');
 
-  expect(app.find('.calc-display').text()).toEqual('789');
+  expect(app.find('.calc-display').text()).toBe('789');
 });
 
 
@@ -67,23 +67,164 @@ it('7 8 9 - 8 = -> 781', () => {
   const btn8 = row1.find(CalcButton).at(1);
   const btn9 = row1.find(CalcButton).at(2);
 
-  btn7.simulate('click');
-  btn8.simulate('click');
-  btn9.simulate('click');
-
   const row2 = rows.at(2);
   const btnMinus = row2.find(CalcButton).at(3);
-
-  btnMinus.simulate('click');
-
-  btn8.simulate('click');
 
   const row4 = rows.at(4);
   const btnEqual = row4.find(CalcButton).at(2);
 
+  btn7.simulate('click');
+  btn8.simulate('click');
+  btn9.simulate('click');
+  btnMinus.simulate('click');
+  btn8.simulate('click');
   btnEqual.simulate('click');
 
-  expect(app.find('.calc-display').text()).toEqual('781');
+  expect(app.find('.calc-display').text()).toBe('781');
+});
+
+
+it('7 - 8 -> 8', () => {
+  const app = mount(<CalcApp />);
+
+  const rows = app.find('.calc-row');
+
+  const row1 = rows.at(1);
+  const btn7 = row1.find(CalcButton).at(0);
+  const btn8 = row1.find(CalcButton).at(1);
+
+  const row2 = rows.at(2);
+  const btnMinus = row2.find(CalcButton).at(3);
+
+  btn7.simulate('click');
+  btnMinus.simulate('click');
+  btn8.simulate('click');
+
+  expect(app.find('.calc-display').text()).toBe('8');
+});
+
+
+it('7 - + 8 -> 15', () => {
+  const app = mount(<CalcApp />);
+
+  const rows = app.find('.calc-row');
+
+  const row1 = rows.at(1);
+  const btn7 = row1.find(CalcButton).at(0);
+  const btn8 = row1.find(CalcButton).at(1);
+
+  const row2 = rows.at(2);
+  const btnMinus = row2.find(CalcButton).at(3);
+
+  const row3 = rows.at(3);
+  const btnAdd = row3.find(CalcButton).at(3);
+
+  btn7.simulate('click');
+  btnMinus.simulate('click');
+  btnAdd.simulate('click');
+  btn8.simulate('click');
+
+  expect(app.find('.calc-display').text()).toBe('8');
+});
+
+
+it('7 - + 8 - -> 15', () => {
+  const app = mount(<CalcApp />);
+
+  const rows = app.find('.calc-row');
+
+  const row1 = rows.at(1);
+  const btn7 = row1.find(CalcButton).at(0);
+  const btn8 = row1.find(CalcButton).at(1);
+
+  const row2 = rows.at(2);
+  const btnMinus = row2.find(CalcButton).at(3);
+
+  const row3 = rows.at(3);
+  const btnAdd = row3.find(CalcButton).at(3);
+
+  btn7.simulate('click');
+  btnMinus.simulate('click');
+  btnAdd.simulate('click');
+  btn8.simulate('click');
+  btnMinus.simulate('click');
+
+  expect(app.find('.calc-display').text()).toBe('15');
+});
+
+
+it('7 8 9 - 8 - -> 781', () => {
+  const app = mount(<CalcApp />);
+
+  const rows = app.find('.calc-row');
+
+  const row1 = rows.at(1);
+  const btn7 = row1.find(CalcButton).at(0);
+  const btn8 = row1.find(CalcButton).at(1);
+  const btn9 = row1.find(CalcButton).at(2);
+
+  const row2 = rows.at(2);
+  const btnMinus = row2.find(CalcButton).at(3);
+
+  btn7.simulate('click');
+  btn8.simulate('click');
+  btn9.simulate('click');
+  btnMinus.simulate('click');
+  btn8.simulate('click');
+  btnMinus.simulate('click');
+
+  expect(app.find('.calc-display').text()).toBe('781');
+});
+
+
+it('4 * 5 = 0 -> 0', () => {
+  const app = mount(<CalcApp />);
+
+  const rows = app.find('.calc-row');
+
+  const row2 = rows.at(2);
+  const btn4 = row2.find(CalcButton).at(0);
+  const btn5 = row2.find(CalcButton).at(1);
+
+  const row1 = rows.at(1);
+  const btnMultiple = row1.find(CalcButton).at(3);
+
+  const row4 = rows.at(4);
+  const btn0 = row4.find(CalcButton).at(0);
+  const btnEqual = row4.find(CalcButton).at(2);
+
+  btn4.simulate('click');
+  btnMultiple.simulate('click');
+  btn5.simulate('click');
+  btnEqual.simulate('click');
+  btn0.simulate('click');
+
+  expect(app.find('.calc-display').text()).toBe('0');
+});
+
+
+it('1 + 2 = 3 = -> 5', () => {
+  const app = mount(<CalcApp />);
+
+  const rows = app.find('.calc-row');
+
+  const row3 = rows.at(3);
+  const btn1 = row3.find(CalcButton).at(0);
+  const btn2 = row3.find(CalcButton).at(1);
+  const btn3 = row3.find(CalcButton).at(2);
+  const btnAdd = row3.find(CalcButton).at(3);
+
+  const row4 = rows.at(4);
+  const btnEqual = row4.find(CalcButton).at(2);
+
+  btn1.simulate('click');
+  btnAdd.simulate('click');
+  btn2.simulate('click');
+  btnEqual.simulate('click');
+  btn3.simulate('click');
+  btnEqual.simulate('click');
+
+  expect(app.find('.calc-display').text()).toBe('5');
 });
 
 
@@ -104,5 +245,3 @@ it('AC should clear state', () => {
 
   expect(app.state()).toEqual(initialState);
 });
-
-
